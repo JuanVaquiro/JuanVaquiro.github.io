a0a8655a8a6aca32dbb43c63f8bb8d1f4ab02c7e
JuanVaquiro <jvaquiro23@itfip.edu.co>
2023-01-10 18:07:38 -0500

    componet and react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/nintendo-switch.svg'
-import './App.css'
+
+import ControlLeftSwitch from "./components/ControlLeftSwitch";
+import ControlRihgtSwicth from "./components/ControlRihgtSwicth";
+import ScreenConte from "./components/ScreenSwitch";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <section className="flex justify-center items-center">
+      <div className="flex flex-row items-center justify-center p-2 mt-5">
+      <ControlLeftSwitch />
+      <ScreenConte />
+      <ControlRihgtSwicth />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    </section>
+  );
 }
 
-export default App
+export default App;
