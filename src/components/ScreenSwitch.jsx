0d62c9b14ebe704104fd31dae51f7ac08ecf5bbd
JuanVaquiro <jvaquiro23@itfip.edu.co>
2023-01-12 16:21:34 -0500

    clean-code and .content h

--- src/components/ScreenSwitch.jsx
+++ src/components/ScreenSwitch.jsx
@@ -1,11 +1,11 @@
-import icon from "../assets/icon.svg";
 import BtnMedia from "./ButtonsSocialMedia";
-import video from "../assets/bg-video.mp4"
+import video from "../assets/bg-video.mp4";
+import nintendo from "../assets/prg-nintendo.png";
 
 function ScreenConte() {
   return (
     <div className="screen-conter rounded-md border-double border-8 border-gray-500">
-      <video className="p-2" src={video} autoPlay loop muted/>
+      <video className="p-4" src={video} autoPlay loop muted />
       <article className="content p-4">
         <div className="flex flex-col items-center justify-center">
           <div className="flex gap-2">
@@ -17,15 +17,15 @@ function ScreenConte() {
           <span className="text-lime-600 font-title font-black text-4xl p-2">
             Front-end Developer Web Junior
           </span>
-          <p className="text-lg text-center p-2">
+          <p className="text-lg text-center p-2 max-w-prose">
             Un año de experiencia en elaboración de proyectos educativos de
             aplicaciones Web con énfasis en investigación, análisis y desarrollo
-            de software. Tecnólogo en Gestión Informática. Tripulante
-            Certificado de MisiónTIC2022. Me gusta compartir y solventar mis
+            de software. Tecnólogo en Gestión Informática. Me gusta compartir y solventar mis
             habilidades de programador web con conocimientos en computación y
             soluciones Web.
           </p>
-        </div>
+ 
+          </div>
       </article>
     </div>
   );
