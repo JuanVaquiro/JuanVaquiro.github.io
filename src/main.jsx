ae5db133af9ad7b22487e4ffa02ce180288f3433
JuanVaquiro <jvaquiro23@itfip.edu.co>
2023-01-12 16:22:20 -0500

    path contact

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Proyect from "./pages/proyect";
 import Experience from "./pages/experience";
+import Contact from "./pages/contact";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/" element={<App />} />
         <Route path="proyecto" element={<Proyect />} />
         <Route path="experiencia" element={<Experience />} />
+        <Route path="contacto" element={<Contact />} />
       </Routes>
     </HashRouter>
   </React.StrictMode>
