5d8d5e9f11e6bb843391c3c127f65beefd4fc9bc
JuanVaquiro <jvaquiro23@itfip.edu.co>
2023-01-15 14:38:32 -0500

    release page-contact

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,26 +1,22 @@
-import React from 'react'
-import ControlLeftSwitch from '../components/ControlLeftSwitch'
-import ControlRihgtSwicth from '../components/ControlRihgtSwicth'
+import React from "react";
+import ControlLeftSwitch from "../components/ControlLeftSwitch";
+import ControlRihgtSwicth from "../components/ControlRihgtSwicth";
+import FormGmail from "../components/formGmail";
 
 const Contact = () => {
   return (
-    <section className="flex justify-center items-center">
-    <div className="flex flex-row items-center justify-center p-2 mt-5">
-      <ControlLeftSwitch />
-      <div className="screen-conter rounded-md border-double border-8 border-gray-500">
-        <article className="p-4 flex">
-          <div>
-            <div className="flex"></div>
-            <span className="text-green-700 font-title font-black text-2xl">
-             Coctacto
-            </span>
-          </div>
-        </article>
+    <section className="flex justify-center items-center w-full h-screen">
+      <div className="flex flex-row items-center justify-center p-2 mt-5">
+        <ControlLeftSwitch />
+        <div className="screen-conter rounded-md border-double border-8 border-gray-500">
+          <article className="my-7 px-6">
+            <FormGmail />
+          </article>
+        </div>
+        <ControlRihgtSwicth />
       </div>
-      <ControlRihgtSwicth />
-    </div>
-  </section>
-  )
-}
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
